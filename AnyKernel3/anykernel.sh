5691731aa261058a08c8a1010f15ad57aec5222c
kazin <kazin7853@qq.com>
2023-04-29 20:00:07 +0800

    feat: add anykernel3 that can load modules

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,34 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=ExampleKernel by osm0sis @ xda-developers
+# kazin: close devicecheck
+do.devicecheck=0
+do.modules=1
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+
+## boot shell variables
+block=/dev/block/bootdevice/by-name/boot
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
